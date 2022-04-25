d4dccf68f410eba99b51f31df4584ee7af90317e
moushicheng.010 <moushicheng.010@bytedance.com>
2022-04-25 21:32:19 +0800

    Add botConfig.json

--- node-core/index.ts
+++ node-core/index.ts
@@ -1,11 +1,12 @@
 import { bot } from "./instance/bot";
+import config from "./config/bot.config.json";
 
-const cmm = new bot({
-  name: "橙萌萌",
-  host: "http://127.0.0.1:8080",
-  verifyKey: "114514233",
-  qq: 2810704613,
-});
+const cmm=new bot({
+  name: config.name,
+  host: config.host,
+  verifyKey: config.verifyKey,
+  qq: config.qq,
+})
 cmm.start();
 // 退出前向 mirai-http-api 发送释放指令(*)
 process.on("exit", () => {
