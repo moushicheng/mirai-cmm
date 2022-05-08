1fa05ea04ac82544b312030987f78a0172bf8353
moushicheng <1163675107@qq.com>
2022-05-08 15:38:46 +0800

    Fix dailypaper

--- node-core/mods/cmdMod/dailyPaper/index.ts
+++ node-core/mods/cmdMod/dailyPaper/index.ts
@@ -10,37 +10,37 @@ export class dailyPaper implements base {
     this.bot = bot;
   }
   async action() {
-    const imgData=await this.getData();
-    await this.store(imgData)
+    const imgData = await this.getData();
+    await this.store(imgData);
   }
   error() {
     throw new Error("主动抛出错误");
   }
   async getData() {
-    // if(moment().format("h")!=8)return;
-    // const url = await axios
-    //   .get("http://api.soyiji.com//news_jpg")
-    //   .then((res) => {
-    //     return res.data.url;
-    //   });
-    const imgData: any = await axios
-      .get("http://news.soyiji.com/26960-2022-5-02.jpg", { headers: { Referer: "safe.soyiji.com" } })
-      .then((res) => {
-        return res.data;
-      });
+    const imgData = await axios
+        .get("http://news.soyiji.com/26960-2022-5-02.jpg", { headers: { Referer: "safe.soyiji.com" }, responseType:'arraybuffer'})
+        .then((res) => {
+            return res.data
+        });
+        console.log(imgData);
     return imgData
   }
   store(imgData) {
     return new Promise((resolve) => {
-      fs.writeFile(`node-core/statics/img/out/paper.jpeg`, imgData, "binary", (error) => {
-        if (error) {
-          console.log(error);
-          console.log("下载失败");
-        } else {
-          console.log("下载成功！");
+      fs.writeFile(
+        `node-core/statics/img/out/paper.png`,
+        imgData,
+        "binary",
+        (error) => {
+          if (error) {
+            console.log(error);
+            console.log("下载失败");
+          } else {
+            console.log("下载成功！");
+          }
+          resolve(true);
         }
-        resolve(true)
-      });
+      );
     });
   }
 }
