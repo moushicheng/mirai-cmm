47069ea6f6ce2e2b997a726ca9ca17db62187d21
moushicheng <1163675107@qq.com>
2023-10-27 20:26:03 +0800

    feat: 消息传发

--- node-core/index.ts
+++ node-core/index.ts
@@ -1,5 +1,9 @@
-const moduleAlias = require('module-alias')
+
 import path from 'path'
+import Koa,{ Context, Next } from 'koa'
+import Router from 'koa-router'
+import bodyParser from 'koa-bodyparser'
+const moduleAlias = require('module-alias')
 moduleAlias.addAlias('@',path.resolve(__dirname,'../node-core'))
 
 
@@ -24,3 +28,29 @@ process.on("exit", () => {
   cmm.instance.release();
 });
  
+
+//通知模块
+const app = new Koa();
+const router = new Router();
+//转换body请求参数
+app.use(bodyParser());
+
+
+//router配置中间件
+app.use(router.routes());
+app.use(router.allowedMethods());
+router.post('/wx', (ctx: Context,next:Next)=>{
+  const url: string = ctx.request.body.url
+  try{
+    console.log('url')
+    cmm.instance.sendFriendMessage(url,1163675107)
+    cmm.instance.sendFriendMessage(url,993228281)
+  }catch(err){
+    console.log('发送失败')
+  }
+  ctx.body='hello'
+  next();
+})
+
+
+app.listen(3000);
